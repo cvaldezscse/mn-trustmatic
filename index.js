7edf39ba8f9afdb087e8a23e6e5d0d366d6efd83
Carlos Valdez <cvaldez.issc@gmail.com>
2018-10-28 23:51:25 -0600

    Added support to encrypted wallets

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ async function init() {
     await H.timeout.sleep(1000);
     H.console.nMsg(`${H.string.getCurrentDatetime()} Testing RPC Credentials...`);
 
+
+    await H.timeout.sleep(1000);
+    H.console.nMsg(`${H.string.getCurrentDatetime()} Unlocking wallet...`);
+    unlockWallet(null);
+
+
     if (H.operations.equals100Percent()) {
         H.console.nMsg(`${H.string.getCurrentDatetime()} We're ready to pay to the 100% of the reward`);
         H.rpc.getBalance(function (err, resBalance) {
@@ -24,23 +30,34 @@ async function init() {
                     H.console.nMsg(`${H.string.getCurrentDatetime()} Getting rewards received...`);
                     let rewardsNumber = H.operations.getNumberOfRewardsReceived(balance);
                     H.console.nMsg(`${H.string.getCurrentDatetime()} you have ${rewardsNumber}  of rewards to pay...`);
+
                     let listOfAddress = conf.get('addresses');
                     H.console.nMsg(`${H.string.getCurrentDatetime()} Paying now...`);
-                    for(let i = 0; i < listOfAddress.length; i++){
+                    for (let i = 0; i < listOfAddress.length; i++) {
                         let amountToSend = H.operations.getAmountToBeSent(listOfAddress[i].percentage)
-                            ,nameToSend = listOfAddress[i].name
-                            ,addressToSend = listOfAddress[i].address
-                            ,datetime = H.string.getCurrentDatetime();
+                            , nameToSend = listOfAddress[i].name
+                            , addressToSend = listOfAddress[i].address
+                            , datetime = H.string.getCurrentDatetime();
 
-                         H.rpc.sendAmount(addressToSend, amountToSend, function(err, res){
-                            if(!err){
+                        H.rpc.sendAmount(addressToSend, amountToSend, function (err, res) {
+                            if (!err) {
                                 let txid = res.result
-                                  , strMessage = `${datetime} Paid to ${nameToSend} the amount of ${amountToSend} ${conf.get('symbol')} with the txid: ${txid}`
-                                  , strFile = `${datetime} Paid to ${nameToSend} the amount of ${amountToSend} ${conf.get('symbol')} to the address: ${addressToSend} with the txid: ${txid}`;
-                                H.file.appendSingleLineToFile(strFile);
-                                H.console.sMsg(strMessage);
+                                    ,
+                                    strMessage = `${datetime} Paid to ${nameToSend} the amount of ${amountToSend} ${conf.get('symbol')} with the txid: ${txid}`
+                                    ,
+                                    strFile = `${datetime} Paid to ${nameToSend} the amount of ${amountToSend} ${conf.get('symbol')} to the address: ${addressToSend} with the txid: ${txid}`
+                                    ,
+                                    strErrorMsg = `${datetime} Error trying to pay to ${nameToSend} looks like the wallet was not unlocked successfully`;
+
+                                if(txid){
+                                    H.file.appendSingleLineToFile(strFile);
+                                    H.console.sMsg(strMessage);
+                                } else {
+                                    H.file.appendSingleLineToFile(strErrorMsg);
+                                    H.console.eMsg(strErrorMsg);
+                                }
                             }
-                            else{
+                            else {
                                 H.console.eMsg(`${datetime} There was an error trying to pay to ${nameToSend} with the next error: ${err.message}`);
                             }
                         });
@@ -56,6 +73,14 @@ async function init() {
         H.console.wMsg(`${H.string.getCurrentDatetime()} DOES NOT Equals 100%, you need to add complete 100% in the addresses`);
     }
 }
+
+function unlockWallet(calback){
+    H.console.wMsg(`${H.string.getCurrentDatetime()} Unlocking wallet ...`);
+    H.rpc.setWalletPassPhrase(conf.get('wallet.passphrase'), conf.get('wallet.timeout'), function(err, res){
+       H.console.sMsg('unlocked') ;
+    });
+}
+
 
 init();
 
